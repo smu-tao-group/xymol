e67619d94753cea9fb5e86b0e98d55eb3ab59987
htian97 <htian1997@gmail.com>
2022-10-06 09:51:05 -0500

    Revert "allow passing str for featurizer to create_map"
    This reverts commit 42c841230050de052e29a4eeda402cc61e07087e.

--- tests/test_create_map.py
+++ tests/test_create_map.py
@@ -10,7 +10,6 @@ from deepchem import deepchem as dc
 
 
 SMILES = "CCC1(CCC(C)C)C(=O)NC(=O)NC1=O"
-
 metric = dc.metrics.mean_absolute_error
 tasks, datasets, transformers = dc.molnet.load_delaney(featurizer='GraphConv', reload=False)
 train_dataset, valid_dataset, test_dataset = datasets
@@ -22,10 +21,10 @@ def test_create_map():
     
     xymol = XYMOL(SMILES)
     file_name = "molecule.png"
-    xymol.create_map("GraphConv", model, file_name)
+    xymol.create_map(dc.feat.ConvMolFeaturizer, model, file_name)
 
-    # check file exists
+    # check file exist
     assert os.path.exists(file_name)
     # remove file
-    os.remove(file_name)
+    os.system(f"rm {file_name}")
 
